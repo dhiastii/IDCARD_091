2f5b759706f63fbab6fd84ee603f822d5001e1e8
dhiastii <dhiastiputri.jayanti@gmail.com>
2022-11-14 21:57:50 +0700

    mengisi request mapping

--- src/main/java/wscidcard/praktikumidcard/controllerdata.java
+++ src/main/java/wscidcard/praktikumidcard/controllerdata.java
@@ -4,10 +4,27 @@
  */
 package wscidcard.praktikumidcard;
 
+import java.io.IOException;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author DELL
  */
+@Controller
 public class controllerdata {
-    
+    @ResponseBody
+    @RequestMapping("/getData")
+    public String getData(@RequestParam("nama") String text,
+                          @RequestParam("image") MultipartFile file,
+                          @RequestParam("lahir")@DateTimeFormat(pattern="yyyy-MM-dd")Date date)
+                          throws IOException {
+        
+    }
 }
