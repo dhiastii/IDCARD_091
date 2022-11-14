8d3c4bff15d4878d4f2f7a34c50b3efd6ad5ff0b
dhiastii <dhiastiputri.jayanti@gmail.com>
2022-11-14 21:25:42 +0700

    pembuatan awal project

---
+++ src/main/java/wscidcard/praktikumidcard/PraktikumidcardApplication.java
@@ -0,0 +1,13 @@
+package wscidcard.praktikumidcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumidcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumidcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wscidcard/praktikumidcard/PraktikumidcardApplicationTests.java
@@ -0,0 +1,13 @@
+package wscidcard.praktikumidcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumidcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
