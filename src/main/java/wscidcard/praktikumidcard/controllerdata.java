18f4a594df87b10676d4d8488c466c910721ab5a
dhiastii <dhiastiputri.jayanti@gmail.com>
2022-11-14 22:36:21 +0700

    membuat langkah menampilkan hasil submit nama, tanggal, dan foto

--- src/main/java/wscidcard/praktikumidcard/controllerdata.java
+++ src/main/java/wscidcard/praktikumidcard/controllerdata.java
@@ -5,9 +5,12 @@
 package wscidcard.praktikumidcard;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,10 +24,22 @@ import org.springframework.web.multipart.MultipartFile;
 public class controllerdata {
     @ResponseBody
     @RequestMapping("/getData")
-    public String getData(@RequestParam("nama") String text,
-                          @RequestParam("image") MultipartFile file,
-                          @RequestParam("lahir")@DateTimeFormat(pattern="yyyy-MM-dd")Date date)
-                          throws IOException {
+    public String getData(@RequestParam("nama") String nama,
+                          @RequestParam("foto") MultipartFile foto,
+                          @RequestParam("lahir")@DateTimeFormat(pattern="yyyy-MM-dd")Date date,
+                          Model model) throws IOException {
         
+    SimpleDateFormat formattgl = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+    String lahir = formattgl.format(date);
+    
+    String blob = Base64.encodeBase64String(foto.getBytes());
+    String fotoo = "data:image/jpeg;base64,".concat(blob);
+    
+    model.addAttribute("nma", nama);
+    model.addAttribute("ttl", lahir);
+    model.addAttribute("fto", foto);
+    
+    return "card";
     }
 }
+
